ce055ec956fac2a92f2612592ea1ed4be0bda08e
Nurlan3007 <maratnurlan3007@gmail.com>
2025-01-17 23:19:33 +0500

    third_commit

--- parserForCategoryInsertDb.py
+++ parserForCategoryInsertDb.py
@@ -4,51 +4,51 @@ import requests
 from bs4 import BeautifulSoup
 from connectToDb import cursor,conn
 
-URL = "https://kingfisher.kz/"
+MAIN_URL = "https://kingfisher.kz/"
 
 headers = {
     "Accept" : '*/*',
     "User-agent" : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
 }
 
-def getPageAndSoup():
+def getPageAndSoup(URL):
     req = requests.get(URL, headers=headers)
     src = req.text
     soup = BeautifulSoup(src, "lxml")
     return soup
 
-soup = getPageAndSoup()
+def getCategoriesAndInsertToDb(soup):
+    dropmenu = soup.find_all(class_="dropmenu")
+    a = soup.find_all(class_="dropmenu a")
+    for items in dropmenu:
+        categoryText = items.find("span").text
+        categoryHref = items.find("span").get('href')
 
-
-dropmenu = soup.find_all(class_="dropmenu")
-a = soup.find_all(class_="dropmenu a")
-for items in dropmenu:
-    categoryText = items.find("span").text
-    categoryHref = items.find("span").get('href')
-
-    cursor.execute("""
-        INSERT INTO categories(category, href) VALUES (%s, %s)
-    """, (categoryText, categoryHref))
-
-    cursor.execute('select id from categories order by id desc limit 1')
-    lastId = cursor.fetchone()[0]
-
-    print(categoryText,categoryHref)
-    sub_categories = items.find_all("a")
-    for sub_category in sub_categories:
-        sub_category_text = sub_category.text
-        sub_category_href = sub_category.get('href')
-        print(sub_category_text, sub_category_href)
-        
         cursor.execute("""
-            INSERT INTO sub_categories(category_id, sub_category, href) VALUES(%s,%s,%s)
-        """, (lastId, sub_category_text, sub_category_href))
+            INSERT INTO categories(category, href) VALUES (%s, %s)
+        """, (categoryText, categoryHref))
+
+        cursor.execute('select id from categories order by id desc limit 1')
+        lastId = cursor.fetchone()[0]
+
+        print(categoryText,categoryHref)
+        sub_categories = items.find_all("a")
+        for sub_category in sub_categories:
+            sub_category_text = sub_category.text
+            sub_category_href = sub_category.get('href')
+            print(sub_category_text, sub_category_href)
+            
+            cursor.execute("""
+                INSERT INTO sub_categories(category_id, sub_category, href) VALUES(%s,%s,%s)
+            """, (lastId, sub_category_text, sub_category_href))
+            
         
-    
-    conn.commit()
-    print('####')
+        conn.commit()
+        print('####')
 
-
+if __name__ ==  'main':
+    soup = getPageAndSoup(MAIN_URL)
+    getCategoriesAndInsertToDb(soup)
 
 
 
--- parserForProducts.py
+++ parserForProducts.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from connectToDb import cursor,conn
+import re
 
 MAIN_URL = "https://kingfisher.kz"
 
@@ -8,7 +9,6 @@ headers = {
     "Accept" : '*/*',
     "User-agent" : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
 }
-
 
 def getPageAndSoup(URL):
     req = requests.get(URL, headers=headers)
@@ -30,30 +30,44 @@ def getCategories():
 # image = soup.find_all("a", class_="image")
 # print(price[0],title[0],descript[0],image[0])
 
+
+def getAllProductsAndInsertToDb(sub_categories):
+    city = 'Almaty'
+    i = 0
+    for sub_category in sub_categories:
+
+        url = MAIN_URL + sub_category[-1]
+        sub_category_id = sub_category[0]
+        print(sub_category_id)
+        
+        soup_sub_category = getPageAndSoup(url)
+        titles = soup_sub_category.find_all("a", class_='title')
+        prices = soup_sub_category.find_all("span", class_='price')
+        descripts = soup_sub_category.find_all("span", class_='descript')
+        images = soup_sub_category.find_all("a", class_="image")
+
+        for j in range(len(titles)):
+            title = titles[j].text.strip()
+            price = prices[j].text.strip()
+            descript = descripts[j].text.strip()
+            image = images[j].get("style")
+
+            price = re.sub(r'[^\d.]', '', price)
+
+            print(title)
+            print(price)
+            print(descript)
+            print(image)
+
+            cursor.execute("""
+                INSERT INTO products (title, image, price, descripts, city, sub_category_id) VALUES(%s,%s,%s,%s,%s,%s)
+            """,(title, image, price, descript, city, sub_category_id))
+
+
+
 sub_categories = getCategories()
-# print(sub_categories)
-# print(sub_categories)
-i = 0
-for sub_category in sub_categories:
-    if i == 3: break
-    url = MAIN_URL + sub_category[-1]
-    print(url)
-    soup_sub_category = getPageAndSoup(url)
-    titles = soup_sub_category.find_all("a", class_='title')
-    prices = soup_sub_category.find_all("span", class_='price')
-    descript = soup_sub_category.find_all("span", class_='descript')
-    image = soup_sub_category.find_all("a", class_="image")
-
-    for j in range(len(titles)):
-        title = titles[j].text
-        price = prices[j].text
-        print(title)
-        print(price)
-        # print()
-        print("#"*30)
-    i += 1
-
-# print(soup)
+getAllProductsAndInsertToDb(sub_categories)
+conn.commit()
 
 
 
